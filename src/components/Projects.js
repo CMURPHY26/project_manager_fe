706ad3f095af6232caa695858bb2bc4dac602772
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-08-22 15:49:10 -0400

    added some more flex box elements and headings for sections

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -27,35 +27,40 @@ const Projects = (props) => {
                     </AccordionItemHeading>
                     <AccordionItemPanel>
                         <div className="project-panel">
-                            <div className="proj-status">
-                                <PieChart style={{ height: '100px' }}
-                                    data={[
-                                        { title: 'One', value: 10, color: '#E38627' },
-                                        { title: 'Two', value: 15, color: '#C13C37' },
-                                        { title: 'Three', value: 20, color: '#6A2135' },
-                                    ]}
-                                />
+                            <div className="proj-progress">
+                                <div className="proj-status">
+                                    <h2>Progress</h2>
+                                    <PieChart style={{ height: '150px' }}
+                                        data={[
+                                            { title: 'One', value: 10, color: '#E38627' },
+                                            { title: 'Two', value: 15, color: '#C13C37' },
+                                            { title: 'Three', value: 20, color: '#6A2135' },
+                                        ]}
+                                    />
                                 </div>
-                            <div className="proj-latest">
-                                <ul>
-                                    <li>Exercitation in fugiat est ut ad ea cupidatat</li>
-                                    <li>ut in cupidatat occaecat ut occaecat consequat</li>
-                                    <li>est minim minim esse tempor laborum consequat esse </li>
-                                    <li>adipisicing eu reprehenderit enim</li>
-                                </ul>
+                                <div className="proj-latest">
+                                    <h3>Latest</h3>
+                                    <ul>
+                                        <li>Exercitation in fugiat est ut ad ea cupidatat</li>
+                                        <li>ut in cupidatat occaecat ut occaecat consequat</li>
+                                        <li>est minim minim esse tempor laborum consequat esse </li>
+                                        <li>adipisicing eu reprehenderit enim</li>
+                                    </ul>
+                                </div>
                             </div>
                             <div className="proj-comments">
+                                <h2>Comments</h2>
                                 <div className="proj-user-comment">
                                     <img src="https://img.icons8.com/nolan/64/dog.png"/>
-                                    <div className="short-comment">Exercitation in fugiat est ut ad ea cupidatat</div>
+                                    <div className="short-comment">Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat</div>
                                 </div>
                                 <div className="proj-user-comment">
                                     <img src="https://img.icons8.com/nolan/64/cat.png"/>
-                                    <div className="short-comment">ut in cupidatat occaecat ut occaecat consequat</div>
+                                    <div className="short-comment">ut in cupidatat occaecat ut occaecat consequat adipisicing eu reprehenderit enim</div>
                                 </div>
                                 <div className="proj-user-comment">
                                     <img src="https://img.icons8.com/nolan/64/dog.png"/>
-                                    <div className="short-comment">adipisicing eu reprehenderit enim</div>
+                                    <div className="short-comment">adipisicing eu reprehenderit enim exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat</div>
                                 </div>
                             </div>
                             <div className="proj-buttons">
