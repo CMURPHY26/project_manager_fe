12fb027e2bd3f5b64ae861f9bd18b98105861153
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-08-22 14:52:10 -0400

    added react pie chart

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,7 +16,7 @@ const Projects = (props) => {
 
     return (
         <div className="container">
-            <Accordion className="project-accordion">
+            <Accordion className="project-accordion" allowZeroExpanded={true}>
                 <AccordionItem>
                     <AccordionItemHeading>
                         <AccordionItemButton>
@@ -24,12 +24,26 @@ const Projects = (props) => {
                         </AccordionItemButton>
                     </AccordionItemHeading>
                     <AccordionItemPanel>
-                        <p>
-                            Exercitation in fugiat est ut ad ea cupidatat ut in
-                            cupidatat occaecat ut occaecat consequat est minim minim
-                            esse tempor laborum consequat esse adipisicing eu
-                            reprehenderit enim.
-                        </p>
+                        <div className="project-panel">
+                            <div className="proj-status">
+
+                            </div>
+                            <div className="proj-latest">
+                                <ul>
+                                    <li>Exercitation in fugiat est ut ad ea cupidatat</li>
+                                    <li>ut in cupidatat occaecat ut occaecat consequat</li>
+                                    <li>est minim minim esse tempor laborum consequat esse </li>
+                                    <li>adipisicing eu reprehenderit enim</li>
+                                </ul>
+                            </div>
+                            <div className="proj-comments">
+
+                            </div>
+                            <div className="proj-buttons">
+                                <img src="https://img.icons8.com/nolan/64/speech-bubble-with-dots.png"/>
+                                <img src="https://img.icons8.com/nolan/64/enter-2.png"/>
+                            </div>
+                        </div>
                     </AccordionItemPanel>
                 </AccordionItem>
                 <AccordionItem>
