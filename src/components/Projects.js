40200773511228342da6bc8f9c332ec3ee7d734b
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-08-22 13:57:16 -0400

    added projects component

---
+++ src/components/Projects.js
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from 'react';
+
+
+
+const Projects = (props) => {
+
+    return (
+        <>
+            
+        </>
+    )
+
+}
+
+export default Projects;
